b3f7329d2d68afb7c344d3ef0e056ccefeee4799
Abhinav <abhinavchaudhary2014@gmail.com>
2025-02-04 16:12:15 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+//App.js
+import React from 'react';
+import './App.css';
+import CompressorComp from './components/Compressor.js';
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+	return <CompressorComp />;
+}
+export default App;
